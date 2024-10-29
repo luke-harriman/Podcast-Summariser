304793532beb02cb9c002e84fc8394450db00e72
luke-harriman <90486632+luke-harriman@users.noreply.github.com>
2024-10-29 11:16:50 +1000

    Update test_requests.py

--- yt_stream_endpoint/endpoint/test_requests.py
+++ yt_stream_endpoint/endpoint/test_requests.py
@@ -3,19 +3,6 @@ import requests
 # from google.oauth2 import service_account
 import subprocess
 
-# The lines below are not needed if you have already set access token and authenticated your enviroment. 
-# key_path = "/Users/lukeh/Desktop/Keys/podcast-summariser-2020-3b7218ef915d.json"
-
-# credentials = service_account.Credentials.from_service_account_file(
-#     key_path,
-#     scopes=["https://www.googleapis.com/auth/cloud-platform"],
-# )
-
-# request = google.auth.transport.requests.Request()
-# credentials.refresh(request)
-# access_token = credentials.token
-
-
 flask_url = "http://127.0.0.1:5000"
 
 # The YouTube URL you want to send
